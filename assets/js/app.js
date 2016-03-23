7627a7cff513cda9e932469648ad355dc968ab43
tillot bertille <bertille@getvalue.com.sg>
2016-03-23 16:58:00 +0100

    addition driver in home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,7 +138,19 @@ app.controller('offerCtrl', ['$scope', '$sails', '$http', '$filter', '$interval'
     var day = date.getDay();
     return day === 0 || day === 6;
   }
-
+  $scope.showAdvanced = function (ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '/templates/dialogDriver.html',
+      parent: angular.element(document.body),
+      targetEvent: ev
+    })
+      .then(function (answer) {
+        $scope.alert = 'You said the information was "' + answer + '".';
+      }, function () {
+        $scope.alert = 'You cancelled the dialog.';
+      });
+  };
 
 }]);
 app.controller('addonCtrl', ['$scope', '$sails', '$http', '$filter', '$interval', '$mdSidenav', '$mdDialog', function ($scope, $sails, $http, $filter, $interval, $mdSidenav, $mdDialog) {
