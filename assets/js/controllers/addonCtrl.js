79beece09cb68007b721b3cdec3b9b36dd01a05f
Clement Chaumette <cchaumette@getvalue.fr>
2016-03-27 23:36:32 +0800

    add-on + dialog

--- assets/js/controllers/addonCtrl.js
+++ assets/js/controllers/addonCtrl.js
@@ -1,47 +1,15 @@
 /**
  * Created by clement on 27/3/16.
  */
-app.controller('addonCtrl', ['$scope', 'policy','product','$filter', '$mdSidenav', '$mdDialog', '$mdMedia', function ($scope,policy,product, $filter, $mdSidenav, $mdDialog, $mdMedia) {
+app.controller('addonCtrl', ['$scope', 'policy','product','plan','$filter', '$mdSidenav', '$mdDialog', '$mdMedia', function ($scope,policy,product,plan, $filter, $mdSidenav, $mdDialog, $mdMedia) {
   console.log("== addonCtrl ==");
+
+  plan.set();
   $scope.policy = policy;
-  $scope.product = product;
-  $scope.plan = product.offer.Products[2].Plans[0];
+  $scope.product = product.selected;
+  $scope.plan = plan.selected;
 
-  $scope.getBenefitsList = [
-    {value:'24/7 Towing & Transportation in Singapore or Overseas', label:'24/7 Towing & transport back home'},
-    {value:'Windscreen Replacement with Excess OR Repair your windscreen at your preferred location and get $50 cash reward with no excess', label:'Windscreen Replacement'},
-    {value:'Guaranteed Repairs for twelve (12) Months', label:'Guaranteed Repairs for 12 Months'},
-    {value:'Delivery of Repaired Car', label:'Delivery of Repaired Car'},
-    //  {value:'Reimbursement of 110% of your cars market value in the event of total loss due to flood (without Basic Own Damage Excess)', label:'110% of your cars market value for total loss due to flood'},
-    {value:'Reimbursement of 110% of your car’s market value in the event of total loss (without Basic Own Damage Excess)', label:'110% of your cars market value for total loss'},
-    {value:'Loss of Personal Effects in Singapore up to $3,000', label:'Loss of Personal Effects'},
-    {value:'Medical and dental expenses up to $1,000 per person for you, your named drivers and your immediate family members', label:'Medical and dental expenses'},
-    {value:'Loss or Damage', label:'Loss or Damage'},
-    {value:'Legal Liability', label:'Legal Liability'},
-    {value:'Daily Transport Allowance of $100 for a maximum of 10 days', label:'Daily Transport Allowance'},
-    {value:'Workshop of Your Choice', label:'Workshop of Your Choice'}
-  ];
-
-  $scope.getPacks = function () {
-    var info;
-    for (var i = 0; i < $scope.plan.Packs.length; ++i) {
-      info = $scope.getMarketingInfo($scope.plan.Packs[i].MarketingInfo.RecordKey);
-      $scope.plan.Packs[i]['Benefits'] = [];
-      for (var j = 0; j < info.Fields.length; ++j) {
-        if (info.Fields[j].Name.indexOf("Main Title") > -1) {
-          $scope.plan.Packs[i]['Title']= info.Fields[j].Value ;
-        }
-        else if(info.Fields[j].Name.indexOf("Sub Title 1") > -1){
-          $scope.plan.Packs[i]['SubTitle']= info.Fields[j].Value ;
-        }
-        else if (info.Fields[j].Name.indexOf("Key Benefit") > -1 && info.Fields[j].Value) {
-          $scope.plan.Packs[i]['Benefits'].push(info.Fields[j].Value);
-        }
-      }
-    }
-  }
-  $scope.getPacks();
-
+/*
   $scope.getAddons = function () {
     var info;
     for (var i = 0; i < $scope.plan.AddOns.length; ++i) {
@@ -59,7 +27,7 @@ app.controller('addonCtrl', ['$scope', 'policy','product','$filter', '$mdSidenav
 
   $scope.getAddons();
 
-
+*/
 
   $scope.policy.packs = [
     {id:'family', name:'Family protector', selected : false, panel_content:"panel content for FP", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt quem offendit, cillum proident ne reprehobant voluptatibus quo mentitum est laboris. Quorum mandaremus graviterque. Mentitum id velit, dolor aut litteris, ea varias illustriora, ita commodo ita ingeniis, iis nulla appellat incurreret, aut irure amet summis pariatur ita ubi quis dolore veniam proident, consequat sed ingeniis."},
--- assets/js/services/planFactory.js
+++ assets/js/services/planFactory.js
@@ -1,15 +1,54 @@
 /**
  * Created by clement on 27/3/16.
  */
-app.factory('plan', function(product) {
+app.factory('plan', function(product, marketing) {
   console.log("===factory.plan==");
   var plan = {};
-  plan.data = null;
+  plan.selected = null;
+  plan.setSelected = function (){
+      if(product.selected) {
 
-  plan.setFromProduct = function (){
+        console.log( product.selected.Plans);
+        for (var i = 0; i < product.selected.Plans.length; i++) {
+          if(product.selected.Plans[i].IsSelected === "true" || product.selected.Plans[i].IsSelected === true ) {
+            plan.selected =  product.selected.Plans[i];
+            return
+          }
+        }
+      }
+    }
+
+
+  plan.setPacks = function () {
+    var mkt;
+    for (var i = 0; i < plan.selected.Packs.length; ++i) {
+      mkt = marketing.get(plan.selected.Packs[i].MarketingInfo.RecordKey);
+      plan.selected.Packs[i]['__Benefits'] = {};
+      plan.selected.Packs[i]['__Benefits']['Benefits'] = [];
+      for (var j = 0; j < mkt.Fields.length; ++j) {
+        if (mkt.Fields[j].Name.indexOf("Main Title") > -1) {
+          plan.selected.Packs[i]['__Benefits']['Title']= mkt.Fields[j].Value ;
+        }
+        else if(mkt.Fields[j].Name.indexOf("Sub Title 1") > -1){
+          plan.selected.Packs[i]['__Benefits']['SubTitle']= mkt.Fields[j].Value ;
+        }
+        else if (mkt.Fields[j].Name.indexOf("Key Benefit") > -1 && mkt.Fields[j].Value) {
+          plan.selected.Packs[i]['__Benefits']['Benefits'].push(mkt.Fields[j].Value);
+        }
+      }
+    }
+  }
 
 
 
+
+  plan.set = function () {
+
+    product.setSelected();
+    plan.setSelected();
+    console.log("==plan selected==");
+    console.log( plan.selected);
+    plan.setPacks();
   }
 
   return plan;
