79e868da8592592f121edda331620765403158ce
Clement Chaumette <cchaumette@getvalue.fr>
2016-03-27 23:51:22 +0800

    addon new style

--- assets/js/controllers/addonCtrl.js
+++ assets/js/controllers/addonCtrl.js
@@ -9,45 +9,11 @@ app.controller('addonCtrl', ['$scope', 'policy','product','plan','$filter', '$md
   $scope.product = product.selected;
   $scope.plan = plan.selected;
 
-/*
-  $scope.getAddons = function () {
-    var info;
-    for (var i = 0; i < $scope.plan.AddOns.length; ++i) {
-      info = $scope.getMarketingInfo($scope.plan.AddOns[i].MarketingInfo.RecordKey);
-      for (var j = 0; j < info.Fields.length; ++j) {
-        if (info.Fields[j].Name.indexOf("Main Title") > -1) {
-          $scope.plan.AddOns[i]['Title']= info.Fields[j].Value ;
-        }
-        else if(info.Fields[j].Name.indexOf("Sub Title 1") > -1){
-          $scope.plan.AddOns[i]['SubTitle']= info.Fields[j].Value ;
-        }
-      }
-    }
-  }
-
-  $scope.getAddons();
-
-*/
-
-  $scope.policy.packs = [
-    {id:'family', name:'Family protector', selected : false, panel_content:"panel content for FP", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt quem offendit, cillum proident ne reprehobant voluptatibus quo mentitum est laboris. Quorum mandaremus graviterque. Mentitum id velit, dolor aut litteris, ea varias illustriora, ita commodo ita ingeniis, iis nulla appellat incurreret, aut irure amet summis pariatur ita ubi quis dolore veniam proident, consequat sed ingeniis."},
-    {id:'duo', name:'Pack duo', selected : false, panel_content:"panel content for duo", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"},
-    //  {id:'overseas', name:'Overseas protector', selected : false, panel_content:"panel content for overseas protector", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"},
-    //  {id:'claims', name:'Claims protector', selected : false, panel_content:"panel content for claims", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"}
-  ];
-  $scope.policy.addons = [
-    {id:'excess', name:'Family protector', selected : false, panel_content:"panel content for FP", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt quem offendit, cillum proident ne reprehobant voluptatibus quo mentitum est laboris. Quorum mandaremus graviterque. Mentitum id velit, dolor aut litteris, ea varias illustriora, ita commodo ita ingeniis, iis nulla appellat incurreret, aut irure amet summis pariatur ita ubi quis dolore veniam proident, consequat sed ingeniis."},
-    {id:'ncd protector', name:'Pack duo', selected : false, panel_content:"panel content for duo", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"},
-    {id:'overseas', name:'Overseas protector', selected : false, panel_content:"panel content for overseas protector", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"},
-    {id:'claims', name:'Claims protector', selected : false, panel_content:"panel content for claims", modal_body:  "Fore aut non quem incididunt, varias reprehenderit deserunt que"}
-  ];
-
   $scope.$watch(function() {
     return $mdMedia('xs') || $mdMedia('sm');
   }, function(wantsFullScreen) {
     $scope.customFullscreen = (wantsFullScreen === true);
   });
-
 
   $scope.showPacks = function(ev, p) {
     console.log("== showPacks ==");
@@ -62,7 +28,4 @@ app.controller('addonCtrl', ['$scope', 'policy','product','plan','$filter', '$md
       locals :{currentPack : p }
     })
   };
-
-
-
 }]);
--- assets/js/services/planFactory.js
+++ assets/js/services/planFactory.js
@@ -18,37 +18,49 @@ app.factory('plan', function(product, marketing) {
       }
     }
 
-
   plan.setPacks = function () {
     var mkt;
     for (var i = 0; i < plan.selected.Packs.length; ++i) {
       mkt = marketing.get(plan.selected.Packs[i].MarketingInfo.RecordKey);
-      plan.selected.Packs[i]['__Benefits'] = {};
-      plan.selected.Packs[i]['__Benefits']['Benefits'] = [];
+      plan.selected.Packs[i]['__label'] = {};
+      plan.selected.Packs[i]['__label']['Benefits'] = [];
       for (var j = 0; j < mkt.Fields.length; ++j) {
         if (mkt.Fields[j].Name.indexOf("Main Title") > -1) {
-          plan.selected.Packs[i]['__Benefits']['Title']= mkt.Fields[j].Value ;
+          plan.selected.Packs[i]['__label']['Title']= mkt.Fields[j].Value ;
         }
         else if(mkt.Fields[j].Name.indexOf("Sub Title 1") > -1){
-          plan.selected.Packs[i]['__Benefits']['SubTitle']= mkt.Fields[j].Value ;
+          plan.selected.Packs[i]['__label']['SubTitle']= mkt.Fields[j].Value ;
         }
         else if (mkt.Fields[j].Name.indexOf("Key Benefit") > -1 && mkt.Fields[j].Value) {
-          plan.selected.Packs[i]['__Benefits']['Benefits'].push(mkt.Fields[j].Value);
+          plan.selected.Packs[i]['__label']['Benefits'].push(mkt.Fields[j].Value);
         }
       }
     }
   }
 
-
-
+  plan.setAddons = function () {
+   var mkt;
+   for (var i = 0; i < plan.selected.AddOns.length; ++i) {
+     mkt = marketing.get(plan.selected.AddOns[i].MarketingInfo.RecordKey);
+     plan.selected.AddOns[i]['__label'] = {};
+     for (var j = 0; j < mkt.Fields.length; ++j) {
+       if (mkt.Fields[j].Name.indexOf("Main Title") > -1) {
+         plan.selected.AddOns[i]['__label']['Title']= mkt.Fields[j].Value ;
+       }
+       else if(mkt.Fields[j].Name.indexOf("Sub Title 1") > -1){
+         plan.selected.AddOns[i]['__label']['SubTitle']= mkt.Fields[j].Value ;
+       }
+     }
+    }
+  }
 
   plan.set = function () {
-
     product.setSelected();
     plan.setSelected();
     console.log("==plan selected==");
     console.log( plan.selected);
     plan.setPacks();
+    plan.setAddons();
   }
 
   return plan;
